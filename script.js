0c988e95dcb5605cc9e17001afd1aae7a3c97175
Arnobmm <arnobjj22@gmail.com>
2025-02-27 12:11:12 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Get the button element
+const button = document.getElementById('alertButton');
+
+// Add an event listener to show an alert when the button is clicked
+button.addEventListener('click', function() {
+    alert('Hello, welcome to my website!');
+});
