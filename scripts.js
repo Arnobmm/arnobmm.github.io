7e3c955dabe988b0076ff7ee7d69a0bf95782dc6
Arnobmm <arnobjj22@gmail.com>
2025-02-28 15:06:05 +0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,4 @@
-
 document.getElementById("contact-form").addEventListener("submit", function(event) {
     event.preventDefault();
-    
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let message = document.getElementById("message").value;
-    
-    document.getElementById("response").innerText = `Thanks, ${name}! I will get back to you at ${email}.`;
-    
-    this.reset();
+    document.getElementById("response").innerText = "Thanks for your message!";
 });
